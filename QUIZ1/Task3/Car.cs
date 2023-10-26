7e34cc75595cb27a1fbae706eadecace629e79da
NKH9 <94634886+NKH9@users.noreply.github.com>
2023-10-26 15:12:31 +0400

    Add files via upload

---
+++ QUIZ1/Task3/Car.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quiz2_NikaKhorbaladze.Task3
+{
+    public class Car : Vehicle
+    {
+        public override void StartEngine()
+        {
+            Console.WriteLine("Car engine started");
+        }
+
+        public override void Accelerate()
+        {
+            Console.WriteLine("Car is accelerating");
+        }
+    }
+}
---
+++ QUIZ1/Task3/Vehicle.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quiz2_NikaKhorbaladze.Task3
+{
+    public abstract class Vehicle
+    {
+        public abstract void StartEngine();
+        public abstract void Accelerate();
+    }
+}
